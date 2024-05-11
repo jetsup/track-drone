2407a81ca688940c135ea880d2c11c4b62981389
George Ngigi <ngigegeorge023@gmail.com>
2024-05-11 04:36:10 +0300

    Moved Port and Baudrate select to a separate file.

---
+++ config.example.js
@@ -0,0 +1,4 @@
+let SERIAL_PORT = ""; // "/dev/ttyUSBx" | "/dev/ttyACMx" - linux // "COMx" - Windows
+let BAUDRATE = 115200;
+
+module.exports = { port:SERIAL_PORT, baud:BAUDRATE };
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
 
+const config = require("./config");
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
@@ -15,68 +17,61 @@ app.use("/js", express.static(__dirname + "/node_modules/"));
 let BASE_LAT, BASE_LON, BASE_ALT;
 
 // Replace with your device port name and baud rate
-const portName = "/dev/ttyUSB1";
-const baudRate = 115200;
+const portName = config.port; // "/dev/ttyUSBx"|"/dev/ttyACMx" or "COMx";
+const baudRate = config.baud; // 115200;
 const serialPort = new SerialPort({
   path: portName,
   baudRate: baudRate,
 });
 let serialBuffer = "";
 serialPort.on("data", (data) => {
-  serialBuffer += data.toString(); // Append incoming data to the buffer
+  try {
+    const message = data.toString().replace("<", "").replace(">", "").trim();
+    // base data|dynamic data
+    let base = message.split("|")[0].trim().split(",");
+    BASE_LAT = Number(base[0]);
+    BASE_LON = Number(base[1]);
+    BASE_ALT = Number(base[2]);
+    let msgArray = message.split("|")[1].trim().split(",");
 
-  const newlineIndex = serialBuffer.indexOf("\n");
-  if (newlineIndex !== -1) {
-    try {
-      const message = serialBuffer
-        .slice(0, newlineIndex)
-        .replace("<$", "")
-        .replace("#>", "");
-      // serialBuffer.replace("<$", "").replace("#>", "");
-      console.log("%%>", message);
-      serialBuffer = serialBuffer.slice(newlineIndex + 1);
-
-      // base data|dynamic data
-      let base = message.split("|")[0].split(",");
-      BASE_LAT = Number(base[0]);
-      BASE_LON = Number(base[1]);
-      BASE_ALT = Number(base[2]);
-      let msgArray = message.split("|")[1].split(",");
-
-      console.log("Received data:", ...msgArray, ":BASE:", ...base);
-
-      if (msgArray.length < 5) {
-        return;
-      }
-
-      let lat = parseFloat(msgArray[0]);
-      let lon = parseFloat(msgArray[1]);
-      let mTime = msgArray[2];
-      let alt = parseFloat(msgArray[3]);
-      let speed = parseFloat(msgArray[4]);
-      let course = parseFloat(msgArray[5]);
-      let distance = parseFloat(msgArray[6]);
-      // io.emit("serialData", { lat, lon, alt, speed, mTime, distance }); // Emit data to connected clients
-      io.emit("serialData", [
-        {
-          lat: lat,
-          lng: lon,
-          baseLat: BASE_LAT,
-          baseLng: BASE_LON,
-          mTime: mTime,
-          alt: alt,
-          baseAlt: BASE_ALT,
-          speed: speed,
-          course: course,
-          distance: distance,
-        },
-      ]); // Emit data to connected clients
-
-      serialBuffer = "";
-    } catch (error) {
-      console.log("Error parsing data:", error.message);
+    if (base.length != 3 && msgArray.length != 7) {
+      throw RangeError("The length of the data is invalid!");
     }
+    console.log("Received data:", msgArray, ":BASE:", base);
+
+    if (msgArray.length < 5) {
+      return;
+    }
+
+    let lat = parseFloat(msgArray[0]);
+    let lon = parseFloat(msgArray[1]);
+    let mTime = msgArray[2];
+    let alt = parseFloat(msgArray[3]);
+    let speed = parseFloat(msgArray[4]);
+    let course = parseFloat(msgArray[5]);
+    let distance = parseFloat(msgArray[6]);
+    // io.emit("serialData", { lat, lon, alt, speed, mTime, distance }); // Emit data to connected clients
+    io.emit("serialData", [
+      {
+        lat: lat,
+        lng: lon,
+        baseLat: BASE_LAT,
+        baseLng: BASE_LON,
+        mTime: mTime,
+        alt: alt,
+        baseAlt: BASE_ALT,
+        speed: speed,
+        course: course,
+        distance: distance,
+      },
+    ]); // Emit data to connected clients
+
+    serialBuffer = "";
+  } catch (error) {
+    // console.log("Error parsing data:", error.message);
+    console.log("...");
   }
+  // }
 });
 
 app.get("/", (req, res) => {
